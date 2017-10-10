c88c536f7bf2f6294f13ece91eaef6147ceab1b5
Julien Coulon <julien.j.coulon@icloud.com>
2017-10-10 22:39:51 +0200

    Removed all files that are in the .gitignore

--- dist/models/Asset.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Asset {
-    constructor(name, percentage) {
-        this.name = name;
-        this.percentage = percentage;
-    }
-}
-exports.Asset = Asset;
